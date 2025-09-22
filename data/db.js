f3b9f0f3e888d7f7fcb05f90be46c6ebc1981bbe
MelaniaPicone <melania_picone@yahoo.it>
2025-09-22 20:47:14 +0200

    [feat] db & env

---
+++ data/db.js
@@ -0,0 +1,24 @@
+// importiamo mysql2
+
+const mysql2 = require("mysql2");
+
+// creo la connessione
+const connection = mysql.createConnection(
+{
+host: "localhost",
+user: "root",
+password: "password",
+database: "db_movies",
+port: 3307
+});
+
+// stabilisco la connessione al db
+connection.connect((err) => {
+if (err) {
+  console.log(`Errore nella connessione al db ${err}`)
+}
+
+else {
+console.log("Connessione al db avvenuta correttamente")
+}
+});
