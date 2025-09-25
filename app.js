12b0c0cc26268b154351c36e6fe42eca9342da56
MelaniaPicone <melania_picone@yahoo.it>
2025-09-25 13:08:15 +0200

    [add] errorshandler & notfound

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-// importo express e creo l'istanza dell'app
+// importo express
 const express = require('express');
+
+//middlewares
+const imagePathMiddleware = require('./middlewares/imagePathMiddleware');
+
+const notFound = require('./middlewares/notFound');
+
+const errorsHandler = require('./middlewares/errorsHandler')
+
+// istanza dell'app
 const app = express();
 
 // definisco il numero di porta su cui deve girare l'app
@@ -9,6 +18,8 @@ const port = process.env.PORT || 3000;
 const movieRouter = require("./routers/movieRouter");
 
 app.use(express.static('public'));
+
+app.use(imagePathMiddleware);
 
 // definisco la rotta base
 app.get("/", (req, res) => {
